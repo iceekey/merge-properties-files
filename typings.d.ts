27729ad2ac7fb1529226c42bec47015fb9fd67e4
Dmitry Meshcheryakov <meshcheryakov@haulmont.com>
2018-03-15 19:06:36 +0400

    Typings changes

--- typings.d.ts
+++ typings.d.ts
@@ -1,2 +1,2 @@
 export declare function mergePropertiesFiles(...sources: string[]): { [key: string]: string};
-export declare function savePropertiesFile(path, properties): Promise<void>;
+export declare function savePropertiesFile(path: string, properties: { [key: string]: string}): Promise<void>;
